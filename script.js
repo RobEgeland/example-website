628787371578647de473a10a93320d0967c4af8b
Robert Egeland <robegeland@yahoo.com>
2021-12-06 17:50:27 -0600

    add script.js, connect to index.html, change style.css

---
+++ script.js
@@ -0,0 +1,6 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+const paragraph = document.createElement("p");
+paragraph.textContent = "I am still test out my Javascript to see how well I know these things but I am very excited to keep going!";
+document.querySelector("body").appendChild(paragraph);
